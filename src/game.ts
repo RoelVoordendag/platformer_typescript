3d2d359d9643f131034eefa3580391e7a6fc71c2
RoelVoordendag <rvoordendag@gmail.com>
2018-05-15 23:05:58 +0200

    Added bump and started with the basics of the project

--- src/game.ts
+++ src/game.ts
@@ -1,14 +1,47 @@
-import PixiSingleton from "./PixiSingleton";
+import * as PIXI from "pixi.js";
+declare var Bump: any;
 
 class Game {
-  constructor() {
-    // initialize canvas
-    PixiSingleton.setInstance(new PIXI.Application(800, 600, { backgroundColor: 0x1099bb }))
-    document.body.appendChild(PixiSingleton.getInstance().view);
+
+  static instance: Game;
+  public pixi: PIXI.Application;
+
+  //singleton function 
+  static getInstance(): Game {
+    if (!Game.instance) { 
+      Game.instance = new Game();
+    }
+    return Game.instance;
+  }
+
+  private constructor() {
+
+    let b = new Bump(PIXI);
+
+    console.log(b);
+    //testing webpack
+    console.log("Hallo");
+    
+    //making the canvas black and setting the canvas full screen
+    this.pixi = new PIXI.Application(innerWidth, innerHeight, {
+      backgroundColor: 0x000000,
+      resolution: window.devicePixelRatio,
+      autoResize: true
+    });
+    PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+
+    document.body.appendChild(this.pixi.view);
 
 
-    console.log("Loaded Game")
+    //setting up gameloop
+    requestAnimationFrame(() => this.gameLoop());
+  }
+  
+  private gameLoop():void{
+
+    requestAnimationFrame(() => this.gameLoop());
   }
 }
-
-window.addEventListener("load", () => new Game())
+window.addEventListener("load", () => {
+  Game.getInstance();
+})
