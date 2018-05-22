6eedb1cb4ff0c1ece96a876f1939d29a2d8ef51d
avandriel <avandriel1998@gmail.com>
2018-05-22 15:32:32 +0200

    Added player, movement and collision between player and tiles

--- src/World/Tile.ts
+++ src/World/Tile.ts
@@ -3,29 +3,33 @@ import Game from "../Game"
 export default class Tile{
     public x:number;
     public y:number;
+    public width: number = 64
+    public heigth: number = 64
     public texture:string;
     private sprite: PIXI.Sprite;
         
     constructor(x:number , y:number , texture:string ){
-        
-        //x and y
         this.x = x;
         this.y = y;
         this.texture = texture;
-
         let id:any = PIXI.loader.resources["tileset"].textures; 
-
         this.sprite = new PIXI.Sprite(id[this.texture]);
-
-        this.sprite.x = this.x;
-
-        console.log(this.y);
-        
+        this.sprite.x = this.x; 
         this.sprite.y = window.innerHeight - this.sprite.height * this.y;
         
-        console.log("tile is created" + "y=" + this.sprite.y + "x=" + this.sprite.x );
-
-        console.log(this.texture);
         Game.getInstance().pixi.stage.addChild(this.sprite);
+    }
+
+    //check collision between given sprite and tiles
+    isColliding(x: number, y: number): boolean {          
+        if (
+            x + 64 > this.x
+            && x < this.x + this.width
+            && y + 64 > this.sprite.y
+            && y < this.sprite.y + this.heigth
+        ) {
+            return true
+        }
+        return false
     }
 }
